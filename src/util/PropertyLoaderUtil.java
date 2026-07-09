18358e32828920625e00ecd5f899c15f504e3a3f
Kishore S <kishore279k@gmail.com>
2026-07-10 00:13:24 +0530

    feat: improve deployment configuration

--- src/util/PropertyLoaderUtil.java
+++ src/util/PropertyLoaderUtil.java
@@ -1,18 +1,10 @@
 package util;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PropertyLoaderUtil {
 
     private static final Logger logger =
             Logger.getLogger(PropertyLoaderUtil.class.getName());
-    private static final String ACTIVE_PROFILE = System.getProperty("profile", "win");
-    private static final String CONFIG_FILE    = "/db-" + ACTIVE_PROFILE + "-config.properties";
-
     private PropertyLoaderUtil() {}
 
     public static void loadDBProperties(DBConfigProperties dbConfigProperties) {
